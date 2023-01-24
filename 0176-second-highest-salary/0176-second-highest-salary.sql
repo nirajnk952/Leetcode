7dce14b456f13c337c0e1474c30e0c66751bcef3
Niraj Kumar <59105884+nirajnk952@users.noreply.github.com>
2023-01-24 17:44:36 +0530

    Time: 433 ms (24.72%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,9 @@
 SELECT max(Salary) 
 as SecondHighestSalary
 FROM Employee
-WHERE Salary < (SELECT max(Salary) FROM Employee)
+WHERE Salary < (SELECT max(Salary) FROM Employee) 
+
+
+
+/*select max(salary) from Employee 
+where salary < ( select max(salary) from Employee);*/
